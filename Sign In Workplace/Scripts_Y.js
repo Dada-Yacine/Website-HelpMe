fb975b73426539df5a7cd02934b019c758445ddf
El-Depressedo <84091583+El-Depressedo@users.noreply.github.com>
2021-05-30 17:24:58 +0100

    Islam Samt

--- Sign In Workplace/Scripts_Y.js
+++ Sign In Workplace/Scripts_Y.js
@@ -1,4 +1,4 @@
-/* -- Sign In Form -------------------------------------------------------------------- */
+/* -- Sign In Form - Check Email Format -------------------------------------------------------------------- */
 (function($) {
   "use strict";
   var input = $('.validate-input .input100');
@@ -12,7 +12,6 @@
     }
     return check;
   });
-
   $('.validate-form .input100').each(function() {
     $(this).focus(function() {
       hideValidate(this);
@@ -20,7 +19,7 @@
   });
 
   function validate(input) {
-    if ($(input).attr('type') == 'text' || $(input).attr('name') == 'email') {
+    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
       if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
         return false;
       }
@@ -33,6 +32,7 @@
 
   function showValidate(input) {
     var thisAlert = $(input).parent();
+
     $(thisAlert).addClass('alert-validate');
   }
 
@@ -41,29 +41,142 @@
     $(thisAlert).removeClass('alert-validate');
   }
 })(jQuery);
-/* -- End Sign In Form -------------------------------------------------------------------- */
+/* -- End Sign In Form - Check Email Format -------------------------------------------------------------------- */
 
 
-/* -- Drag & Drop --------------------------------------------------------------------
+/* -- Sign In Form - Check Number Format -------------------------------------------------------------------- */
+(function($) {
+  "use strict";
+  var input = $('.validate-input .input100');
+  $('.validate-form').on('submit', function() {
+    var check = true;
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
+    return check;
+  });
+  $('.validate-form .input100').each(function() {
+    $(this).focus(function() {
+      hideValidate(this);
+    });
+  });
 
-FilePond.registerPlugin(
+  function validate(input) {
+    if ($(input).attr('type') == 'number' || $(input).attr('name') == 'n_telephone') {
+      if ($(input).val().trim().match(/([0-9]{10})/) == null) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == '') {
+        return false;
+      }
+    }
+  }
 
-	// encodes the file as base64 data
-  FilePondPluginFileEncode,
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
 
-	// validates the size of the file
-	FilePondPluginFileValidateSize,
+    $(thisAlert).addClass('alert-validate');
+  }
 
-	// corrects mobile image orientation
-	FilePondPluginImageExifOrientation,
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
+  }
+})(jQuery);
+/* -- End Sign In Form - Check Number Format -------------------------------------------------------------------- */
 
-	// previews dropped images
-  FilePondPluginImagePreview
-);
 
-// Select the file input and use create() to turn it into a pond
-FilePond.create(
-	document.querySelector('input')
-);
+/* -- Sign In Form - Check PayPal Format -------------------------------------------------------------------- */
+(function($) {
+  "use strict";
+  var input = $('.validate-input .input100');
+  $('.validate-form').on('submit', function() {
+    var check = true;
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
+    return check;
+  });
+  $('.validate-form .input100').each(function() {
+    $(this).focus(function() {
+      hideValidate(this);
+    });
+  });
 
--- End Drag & Drop -------------------------------------------------------------------- */
+  function validate(input) {
+    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email_paypal') {
+      if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == '') {
+        return false;
+      }
+    }
+  }
+
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
+  }
+})(jQuery);
+/* -- End Sign In Form - Check PayPal Format -------------------------------------------------------------------- */
+
+
+/* -- Sign In Form - Check CCP Number Format -------------------------------------------------------------------- */
+(function($) {
+  "use strict";
+  var input = $('.validate-input .input100');
+  $('.validate-form').on('submit', function() {
+    var check = true;
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
+    return check;
+  });
+  $('.validate-form .input100').each(function() {
+    $(this).focus(function() {
+      hideValidate(this);
+    });
+  });
+
+  function validate(input) {
+    if ($(input).attr('type') == 'number' || $(input).attr('name') == 'ccp') {
+      if ($(input).val().trim().match(/(([0-9]{7})+(\ )+(\-)+(\ )+([0-9]{2}))/) == null) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == '') {
+        return false;
+      }
+    }
+  }
+
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
+  }
+})(jQuery);
+/* -- End Sign In Form - Check CCP Number Format -------------------------------------------------------------------- */
